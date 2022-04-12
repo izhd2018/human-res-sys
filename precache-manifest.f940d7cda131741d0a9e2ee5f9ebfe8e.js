8778f1cd950f66a304bb7da64a347ddd433c1658
Himanshu Midha <jeeshan.1207@gmail.com>
2022-04-12 15:33:58 +0530

    initial commit

---
+++ precache-manifest.f940d7cda131741d0a9e2ee5f9ebfe8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1586c046c3ced6af6e93f786a8550b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b4477441b30bb8cf7f",
+    "url": "/static/css/main.0d807116.chunk.css"
+  },
+  {
+    "revision": "14b25fd050409c523701",
+    "url": "/static/js/2.410a2538.chunk.js"
+  },
+  {
+    "revision": "1dd0d5b4318da7e74c0c4744de1842fd",
+    "url": "/static/js/2.410a2538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b4477441b30bb8cf7f",
+    "url": "/static/js/main.56725dae.chunk.js"
+  },
+  {
+    "revision": "9030c7668e656f2f612a",
+    "url": "/static/js/runtime-main.6c5be910.js"
+  }
+]);
